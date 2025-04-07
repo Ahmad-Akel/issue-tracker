931275f1f9076c92e5125dd2914369e4a8f6c5dc
ahmadakel <ahmad.oudai1999@gmail.com>
2025-04-07 19:44:58 +0200

    Style the navbar links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,13 +1,18 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
+import { usePathname } from "next/navigation";
+import classnames from "classnames";
 
 export const NavBar = () => {
+  const pathName = usePathname();
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
-
+  console.log(pathName);
   return (
     <nav className="flex space-x-6 mb-5 border-b px-5 h-14 items-center">
       <Link href="/">
@@ -18,7 +23,11 @@ export const NavBar = () => {
           <Link
             key={link.href}
             href={link.href}
-            className="text-zinc-50 hover:text-zinc-500 transition-colors"
+            className={classnames({
+              "text-zinc-50": link.href === pathName,
+              "text-zinc-400": link.href !== pathName,
+              "hover:text-zinc-500": true,
+            })}
           >
             {link.label}
           </Link>
